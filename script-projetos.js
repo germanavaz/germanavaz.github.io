22de28a16cbdf4026761cb3f2e522b319ca22535
germanavaz <gmsvaz@gmail.com>
2023-09-14 21:46:05 -0300

    alterações css página projetos

--- script-projetos.js
+++ script-projetos.js
@@ -3,29 +3,41 @@ const categorias = document.querySelectorAll('input[name="categoria"]');
 const buscaInput = document.getElementById('busca');
 const resultados = document.getElementById('resultados');
 
-// Adicionar um evento de clique a cada categoria
-categorias.forEach(categoria => {
-  categoria.addEventListener('change', filtrarResultados);
-});
+// Exemplo de lista de resultados 
+const listaResultados = [
+  { titulo: 'Lading-page ODS', imagem: './imagens/projetos/ods.png', link: 'https://germanavaz.github.io/ods-onu/', categoria: 'html' },
+  { titulo: 'Exemplo CSS', categoria: 'css' },
+  { titulo: 'Exemplo JavaScript', categoria: 'javascript' },
+  { titulo: 'Exemplo WordPress', categoria: 'wordpress' },
+  { titulo: 'Exemplo Elementor', categoria: 'elementor' },
+  { titulo: 'Exemplo Outra Categoria', categoria: 'outra' },
+];
 
-// Adicionar um evento de input para a busca
-buscaInput.addEventListener('input', filtrarResultados);
+// Função para exibir todos os resultados iniciais
+function exibirTodosResultados() {
+  resultados.innerHTML = '';
+  listaResultados.forEach(resultado => {
+    const divResultado = document.createElement('div');
+    divResultado.innerHTML =  `<div class="projeto-cadaprojeto"> <h3>${resultado.titulo}</h3>
+    <a href="${resultado.link}" target="_blank">
+      <img src="${resultado.imagem}" alt="${resultado.titulo}" width="100">
+    </a></div>
+  `;
+    resultados.appendChild(divResultado);
+  });
+}
 
-// Função para filtrar os resultados
+// Função para filtrar os resultados com base nas categorias selecionadas e no termo de busca
 function filtrarResultados() {
   const termoBusca = buscaInput.value.toLowerCase();
   const categoriasSelecionadas = Array.from(categorias)
     .filter(categoria => categoria.checked)
     .map(categoria => categoria.value);
 
-  // Aqui você pode adicionar sua lógica de filtragem com base no termo de busca e nas categorias selecionadas
-  // Por exemplo, você pode ter uma lista de resultados e mostrar/ocultar de acordo com os critérios
-
   // Limpar resultados anteriores
   resultados.innerHTML = '';
 
   // Exibir resultados correspondentes
-  // Suponha que você tenha uma lista de resultados em um array chamado 'listaResultados'
   listaResultados.forEach(resultado => {
     if (
       resultado.titulo.toLowerCase().includes(termoBusca) &&
@@ -39,12 +51,13 @@ function filtrarResultados() {
   });
 }
 
-// Exemplo de lista de resultados (você pode substituir isso com seus próprios dados)
-const listaResultados = [
-  { titulo: 'Exemplo HTML', categoria: 'html' },
-  { titulo: 'Exemplo CSS', categoria: 'css' },
-  { titulo: 'Exemplo JavaScript', categoria: 'javascript' },
-  { titulo: 'Exemplo WordPress', categoria: 'wordpress' },
-  { titulo: 'Exemplo Elementor', categoria: 'elementor' },
-  { titulo: 'Exemplo Outra Categoria', categoria: 'outra' },
-];
+// Inicialmente, exiba todos os resultados
+exibirTodosResultados();
+
+// Adicionar um evento de clique a cada categoria
+categorias.forEach(categoria => {
+  categoria.addEventListener('change', filtrarResultados);
+});
+
+// Adicionar um evento de input para a busca
+buscaInput.addEventListener('input', filtrarResultados);
