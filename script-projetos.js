dfe6c915c15b1492b71965fb14880e3d0bdc1a59
germanavaz <gmsvaz@gmail.com>
2023-09-14 22:08:05 -0300

    fix bug filtro

--- script-projetos.js
+++ script-projetos.js
@@ -1,16 +1,40 @@
-// Selecionar os elementos HTML
+/// Selecionar os elementos HTML
 const categorias = document.querySelectorAll('input[name="categoria"]');
-const buscaInput = document.getElementById('busca');
 const resultados = document.getElementById('resultados');
 
-// Exemplo de lista de resultados 
+// Exemplo de lista de resultados (você pode substituir isso com seus próprios dados)
 const listaResultados = [
-  { titulo: 'Lading-page ODS', imagem: './imagens/projetos/ods.png', link: 'https://germanavaz.github.io/ods-onu/', categoria: 'html' },
-  { titulo: 'Exemplo CSS', categoria: 'css' },
-  { titulo: 'Exemplo JavaScript', categoria: 'javascript' },
-  { titulo: 'Exemplo WordPress', categoria: 'wordpress' },
-  { titulo: 'Exemplo Elementor', categoria: 'elementor' },
-  { titulo: 'Exemplo Outra Categoria', categoria: 'outra' },
+  {
+    titulo: 'Exemplo HTML',
+    categoria: 'html',
+    imagem: './imagens/projetos/projetos-mobile/pokedex-mobile.png',
+    link: 'https://germanavaz.github.io/pokedex/'
+  },
+  {
+    titulo: 'Exemplo CSS',
+    categoria: 'css',
+    imagem: './imagens/projetos/projetos-mobile/pokedex-mobile.png',
+    link: 'https://germanavaz.github.io/pokedex/'
+  },
+  {
+    titulo: 'Exemplo JavaScript',
+    categoria: 'javascript',
+    imagem: './imagens/projetos/projetos-mobile/pokedex-mobile.png',
+    link: 'https://germanavaz.github.io/pokedex/'
+  },
+  {
+    titulo: 'Exemplo WordPress',
+    categoria: 'wordpress',
+    imagem: './imagens/projetos/projetos-mobile/pokedex-mobile.png',
+    link: 'https://germanavaz.github.io/pokedex/'
+  },
+  {
+    titulo: 'Exemplo Elementor',
+    categoria: 'elementor',
+    imagem: './imagens/projetos/projetos-mobile/pokedex-mobile.png',
+    link: 'https://germanavaz.github.io/pokedex/'
+  },
+  
 ];
 
 // Função para exibir todos os resultados iniciais
@@ -18,18 +42,29 @@ function exibirTodosResultados() {
   resultados.innerHTML = '';
   listaResultados.forEach(resultado => {
     const divResultado = document.createElement('div');
-    divResultado.innerHTML =  `<div class="projeto-cadaprojeto"> <h3>${resultado.titulo}</h3>
+    divResultado.innerHTML = `
+    <div class="projeto-cadaprojeto">
+    <h3>${resultado.titulo}</h3>
     <a href="${resultado.link}" target="_blank">
       <img src="${resultado.imagem}" alt="${resultado.titulo}" width="100">
     </a></div>
-  `;
+    `;
     resultados.appendChild(divResultado);
   });
 }
 
-// Função para filtrar os resultados com base nas categorias selecionadas e no termo de busca
+// Restante do código permanece o mesmo
+
+// Inicialmente, exiba todos os resultados
+exibirTodosResultados();
+
+// Adicionar um evento de clique a cada categoria
+categorias.forEach(categoria => {
+  categoria.addEventListener('change', filtrarResultados);
+});
+
+// Função para filtrar os resultados com base nas categorias selecionadas
 function filtrarResultados() {
-  const termoBusca = buscaInput.value.toLowerCase();
   const categoriasSelecionadas = Array.from(categorias)
     .filter(categoria => categoria.checked)
     .map(categoria => categoria.value);
@@ -40,24 +75,19 @@ function filtrarResultados() {
   // Exibir resultados correspondentes
   listaResultados.forEach(resultado => {
     if (
-      resultado.titulo.toLowerCase().includes(termoBusca) &&
-      (categoriasSelecionadas.includes(resultado.categoria) ||
-        categoriasSelecionadas.includes('outra'))
+      categoriasSelecionadas.includes(resultado.categoria) ||
+      categoriasSelecionadas.includes('outra')
     ) {
       const divResultado = document.createElement('div');
-      divResultado.textContent = resultado.titulo;
+      divResultado.innerHTML = `
+      <div class="projeto-cadaprojeto">
+        <h3>${resultado.titulo}</h3>
+        <p>Categoria: ${resultado.categoria}</p>
+        <a href="${resultado.link}" target="_blank">
+          <img src="${resultado.imagem}" alt="${resultado.titulo}" width="100">
+        </a> </div>
+      `;
       resultados.appendChild(divResultado);
     }
   });
 }
-
-// Inicialmente, exiba todos os resultados
-exibirTodosResultados();
-
-// Adicionar um evento de clique a cada categoria
-categorias.forEach(categoria => {
-  categoria.addEventListener('change', filtrarResultados);
-});
-
-// Adicionar um evento de input para a busca
-buscaInput.addEventListener('input', filtrarResultados);
