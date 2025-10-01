b42e6bee8dd67a4921c3387dad368aee489bdd9c
germanavaz <gmsvaz@gmail.com>
2025-10-01 10:53:13 -0300

    :wrench: reset

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
 
-function toggleMode() {
-    const html = document.documentElement
-    html.classList.toggle("light")
-  
-  }
-
-  const btnMobile = document.getElementById('btn-mobile');
-
-  function toggleMenu() {
-      const nav = document.getElementById('nav');
-      nav.classList.toggle('active');
-  }
-  
-  btnMobile.addEventListener('click', toggleMenu);
